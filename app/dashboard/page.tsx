9370fbf8d6163dbe3ccb7e6177bc1841db95c0de
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2024-01-02 13:53:58 -0300

    feat: Add rightbar component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,28 @@
+import styles from "./dashboard.module.css"
+
+
+import Card from "@/components/card/Card"
+import Chart from "@/components/chart/Chart"
+import RightBar from "@/components/rightbar/RightBar"
+import Transactions from "@/components/transactions/Transactions"
 import React from "react"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <section className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <aside className={styles.side}>
+        <RightBar />
+      </aside>
+    </section>
   )
 }
 
