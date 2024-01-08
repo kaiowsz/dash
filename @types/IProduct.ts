0301807dfefdfc99beee6d2fae26722972d0c519
Kaio Silveira <107553497+kaiowsz@users.noreply.github.com>
2024-01-08 06:43:12 -0300

    feat: @types

---
+++ @types/IProduct.ts
@@ -0,0 +1,8 @@
+export type IProduct = {
+    title: string;
+    description: string;
+    price: number;
+    img?: string;
+    color?: string;
+    stock: number;
+}
---
+++ @types/IUser.ts
@@ -0,0 +1,11 @@
+export type IUser = {
+    _id: string;
+    img?: string;
+    username: string;
+    email: string;
+    password: string;
+    isActive?: string;
+    isAdmin?: string;
+    phone?: string;
+    address?: string;
+}
